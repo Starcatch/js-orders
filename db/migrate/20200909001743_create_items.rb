becd273deeaba933e060acd100e425a045272d43
Starcatch <timurmishiev@gmail.com>
2020-09-09 23:27:00 -0400

    update gems

--- db/migrate/20200909001743_create_items.rb
+++ db/migrate/20200909001743_create_items.rb
@@ -1,6 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.belongs_to :order, foreign_key: true
+      t.string :item_name
+      t.string :item_price
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Order.create([
+  {client_name: 'John', phone: '345', address: '321'},
+  {client_name: 'Ira', phone: '5', address: '32'},
+  {client_name: 'Les', phone: '8', address: '3'}
+])
+
+order_a = Order.create({client_name: 'bam', phone: '34', address: '31'})
+
+order_a.items.create({item_name: "Challah", item_price: "$3.00"})
+
+
+order_b = Order.create({client_name: 'boom', phone: '77774', address: '3991'})
+
+order_b.items.create({item_name: "Scone", item_price: "$3.00"})
