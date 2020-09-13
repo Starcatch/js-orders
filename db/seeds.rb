a5f341e472c82136d0894752ab91236eb5f0ce4a
Starcatch <timurmishiev@gmail.com>
2020-09-13 18:44:04 -0400

    final touch

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Order.create([
-  {client_name: 'John', phone: '345', address: '321'},
-  {client_name: 'Ira', phone: '5', address: '32'},
-  {client_name: 'Les', phone: '8', address: '3'}
+  {client_name: 'John', phone: '345', address: '321'}
+  
 ])
 
 order_a = Order.create({client_name: 'bam', phone: '34', address: '31'})
@@ -17,6 +16,4 @@ order_a = Order.create({client_name: 'bam', phone: '34', address: '31'})
 order_a.items.create({item_name: "Challah", item_price: "$3.00"})
 
 
-order_b = Order.create({client_name: 'boom', phone: '77774', address: '3991'})
 
-order_b.items.create({item_name: "Scone", item_price: "$3.00"})
